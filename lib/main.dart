010353fbac5c99b11c3c7d7e7e143b7d8b7f00b4
ColaGom <geunho@flitto.com>
2019-01-24 14:14:32 +0900

    feat : gps locator & permission handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() => runApp(MainApp());
+void main() => runApp(DindowApp());
 
-class MainApp extends StatelessWidget {
+const String TITLE = "Dindow";
+
+class DindowApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      title: TITLE,
+      home: DustScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class DustScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _DustScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DustScreenState extends State<DustScreen> {
+  double _dustFactor = 0.0;
+  String _dustDesc = "none";
+  String _positionInfo = "waiting...";
+  Color _iconColor = Colors.grey;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    requestPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(TITLE),
+        elevation: 4.0,
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: ThemeData.light().primaryColor,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.child_care, size: 100.0, color: _iconColor),
+                  Text(
+                    _dustFactor.toString(),
+                    textScaleFactor: 3,
+                  ),
+                  Text(
+                    _dustDesc,
+                    textScaleFactor: 2,
+                  ),
+                  Text(
+                    _positionInfo,
+                    textScaleFactor: 1.2,
+                  )
+                ],
+              ),
+            ) ,
+          )
+        ],
+      ),
+    );
+  }
+
+  void requestPermission() {
+    var requestFuture =
+        PermissionHandler().requestPermissions([PermissionGroup.location]);
+
+    requestFuture.then((result) {
+      if (result[PermissionGroup.location] != PermissionStatus.granted) {
+        setState(() {
+          _dustDesc = "Location permission denied... :(";
+        });
+      } else {
+        requestGps();
+      }
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // i
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void requestGps() {
+    var locator = Geolocator();
+    var options =
+        LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+
+    locator.getCurrentPosition().then((position) {
+      updatePosition(position);
+    });
+
+    locator.getPositionStream(options).listen((position) {
+      updatePosition(position);
+    });
+  }
+
+  void updatePosition(Position position){
+    setState(() {
+      _positionInfo = (position == null) ? "null" : "lat : ${position.latitude}";
+    });
   }
 }
